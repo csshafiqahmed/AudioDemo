48b83e24b3ec6ad7127878b14d548929948c1444
Shafiq Ahmed <csshafiqahmed@gmail.com>
2020-10-14 13:31:08 +0500

    Audio controllers added, Button visibility handled

--- app/src/main/java/com/codestown/audiodemo/MainActivity.java
+++ app/src/main/java/com/codestown/audiodemo/MainActivity.java
@@ -1,20 +1,27 @@
 package com.codestown.audiodemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
     AudioManager audioManager;
 
+    Button pauseButton;
+
     public void play(View view) {
         mediaPlayer.start();
+        pauseButton.setVisibility(View.VISIBLE);
     }
 
     public void pause(View view) {
@@ -27,16 +34,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        int currentvolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        mediaPlayer = MediaPlayer.create(this, R.raw.birds);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        pauseButton = findViewById(R.id.button2);
+        pauseButton.setVisibility(View.INVISIBLE);
 
         SeekBar volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setProgress(currentVolume);
         volumeSeekBar.setMax(maxVolume);
-        volumeSeekBar.setProgress(currentvolume);
+        mediaPlayer = MediaPlayer.create(this, R.raw.birds);
+
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                Log.i("Volume Changed", Integer.toString(progress));
             }
 
             @Override
@@ -49,5 +60,32 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        SeekBar progressSeekBar = findViewById(R.id.seekBar2);
+
+        progressSeekBar.setMax(mediaPlayer.getDuration());
+
+        progressSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                progressSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 300);
     }
 }
